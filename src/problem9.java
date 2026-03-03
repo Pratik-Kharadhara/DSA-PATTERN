511c5f95493cc32ead4484e7481212dd52927a7d
Pratik Kharadhara <pratikkharadhara@gmail.com>
2026-03-04 00:48:23 +0530

    283. Move Zeroes with two pointer

---
+++ src/problem9.java
@@ -0,0 +1,28 @@
+//283. Move Zeroes
+/*Given an integer array nums, move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+Note that you must do this in-place without making a copy of the array.
+*/
+//in this question instead of pushing all the zeros to right push all the non zero to the left
+//then automatically the 0 will be sent to the right
+public class problem9 {
+        public void moveZeroes(int[] nums) {
+            int i= 0;
+            int j =0;//we are intializing it to 0 so that we can test all the edge cases too
+            while(j <= nums.length-1){
+                if(nums[j]!=0){ // when its a non zero we will do the swap and increase the indices
+                    //swap
+                    int temp = nums[i];
+                    nums[i]=nums[j];
+                    nums[j]=temp;
+                    i++;
+                    j++;
+                }
+                else {//if its a zero then just increase the j and keep the i to replace the next non zero
+                    j++;
+                }
+            }
+
+            return;
+        }
+
+}
