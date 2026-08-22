c8c3c6fe5221a1f964fac9e6bf142730e3a4b683
Pratik Kharadhara <pratikkharadhara@gmail.com>
2026-08-23 01:53:09 +0530

    added the rest parking problem

--- src/TCS_NQT_PYQ/problem8.java
+++ src/TCS_NQT_PYQ/problem8.java
@@ -3,6 +3,7 @@ import java.util.*;
 public class problem8 {
   public static void main(String[] args){
       Scanner sc = new Scanner(System.in);
+      int maxOne = Integer.MIN_VALUE;
       System.out.println("Enter the Row");
       int row = sc.nextInt();
 
@@ -12,11 +13,29 @@ public class problem8 {
       int[][] mattrix = new int[row][col];
 
       System.out.println("Enter the Mattrix");
-      for(int i=0;i<=row;i++){
-          for(int j=0;j<=col;j++){
+      for(int i=0;i<row;i++){
+          for(int j=0;j<col;j++){
               mattrix[i][j]=sc.nextInt();
           }
       }
+    int rowNo = 0;
+      for(int i=0;i<row;i++){
+          int crtOne = 0;
+          for(int j=0;j<col;j++){
+              if(mattrix[i][j]==1){
+                  crtOne++;
+                  maxOne = Math.max(crtOne ,maxOne);
+              }
+          }
+          if(maxOne > crtOne){
+              continue;
+          }
+          else if(crtOne >= maxOne){
+              rowNo = i+1;
+          }
+
+      }
+      System.out.println(maxOne+" and the row is : "+ rowNo);
   }
 
 }
